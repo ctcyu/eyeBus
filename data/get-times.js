e32e717b4628d3361987d523b47963744267cc12
Christine Yu <christine.tc.yu@gmail.com>
2012-09-05 03:53:19 -0700

    skeleton started

---
+++ data/get-times.js
@@ -0,0 +1,16 @@
+var stopInput = document.getElementById("stop-input");
+var routeInput = document.getElementById("route-input");
+
+self.port.on("loadStop", function(busStop){
+	stopInput.value = busStop;
+});
+
+self.port.on("loadRoute", function(busRoute){
+	routeInput.value = busRoute;
+});
+
+stopInput.addEventListener('keyup', function onkeyup(event){
+	if(event.keyCode == 13) {
+		self.port.emit("changeStop", stopInput.value);
+	}
+}, false);

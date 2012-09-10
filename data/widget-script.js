fd0a73f00eb3891ae7168806851dfa1738caf9ca
Christine Yu <christine.tc.yu@gmail.com>
2012-09-10 15:04:41 -0700

    added icon

--- data/widget-script.js
+++ data/widget-script.js
@@ -1,10 +1,7 @@
 self.port.on("searchDone", function (message) {
 	var nextDiv = document.getElementById("next-time");
-	var msg = nextDiv.innerHTML = message[0] + ":" + message[1] + "@" + message[2] + "m";
-	console.log(msg);
-	console.log(message[2]);
-	if(message[2] != "") 
-		nextDiv.innerHTML = msg;
-	else
-		nextDiv.innerHTML = "Error loading";
+	var msg = message[0] + ":" + message[1] + "@" + message[2] + "m";
+	console.log("msg:" + msg);
+	console.log("message[2]" + message[2]);
+	nextDiv.innerHTML = msg;
 });
